30364df155de913f63e8abec461bdf20143c8ba7
Shunsuke Ise <ise@ebiiim.com>
2022-11-15 02:01:23 +0900

    scripts

--- hack/dist-manifests.sh
+++ hack/dist-manifests.sh
@@ -8,15 +8,11 @@ SCRIPT=$(realpath "$0")
 PROJECT_ROOT=$(dirname "$(dirname "$SCRIPT")")
 PROJECT_NAME=$(basename "$PROJECT_ROOT")
 
-KIND_CLUSTER_NAME=$PROJECT_NAME
 VERSION=$(git describe --tags --match "v*")
 IMG=$PROJECT_NAME-controller:$VERSION
+DIST_FILE=install.yaml
 
 cd "$PROJECT_ROOT"
 
-make
-make docker-build IMG="$IMG"
-kind load docker-image "$IMG" -n "$KIND_CLUSTER_NAME"
-
-make undeploy || true
-make deploy IMG="$IMG"
+cd config/manager && "$PROJECT_ROOT/bin/kustomize" edit set image controller="$IMG"
+cd "$PROJECT_ROOT" && "$PROJECT_ROOT/bin/kustomize" build config/default > $DIST_FILE
