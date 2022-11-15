ee2fbc36faad9ea87ea8c6c25c586f498fc4bea2
Shunsuke Ise <ise@ebiiim.com>
2022-11-16 03:25:34 +0900

    comments

--- controllers/repository_controller.go
+++ controllers/repository_controller.go
@@ -35,15 +35,7 @@ type RepositoryReconciler struct {
 //+kubebuilder:rbac:groups=batch,resources=cronjobs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;delete
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Repository object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
+// Reconcile moves the current state of the cluster closer to the desired state.
 func (r *RepositoryReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	lg := log.FromContext(ctx)
 	lg.Info("Reconcile")
